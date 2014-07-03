794c511b9b7b0775fd652641631b50d6b15bc9ac
Jake <jhunsaker@redhat.com>
2014-07-03 18:46:07 -0400

    Updated dmidecode parsing. Now with 80% less badness

--- pysos/opsys/biosinfo.py
+++ pysos/opsys/biosinfo.py
@@ -1,19 +1,51 @@
 import os, re
 from config import *
 
+def parse_dmi(dmifile, to_check):
+	with open(dmifile, 'r') as dfile:
+		handle_regex = re.compile('^%s\s'%to_check)
+		newline = re.compile('^$')
+		lines = dfile.readlines()
+		for x in range(0,len(lines)):
+			line = lines[x]
+			if handle_regex.findall(line):
+				
+			# Found header for section
+				section = {}
+				section['info'] = [] 
+				while True:
+					line = lines[x+1]
+			# repeat until we hit newline
+					  
+					if not newline.findall(line):
+						 section['info'].append(line.strip().strip('\t'))
+						 x += 1
+					else:
+						break
+					
+				info = {}
+				for item in section['info']:
+					try:
+						key = item.split(':')[0]
+						value = item.split(':')[1]
+						info[key] = value
+					except:
+						pass
+				return info
+
+
 def get_bios_info(target, local):
 	total_cores = 0
 	total_threads = 0
 	cpus_populated = 0
 	mem_arrays = 0
-	fam_count = 0
-	ver_count = 0
 	total_mem = 0
 	dimm_count = 0
 	empty_dimms = 0
 	core_count = 0
 	thread_count = 0
 	if local:
+		import tempfile, subprocess
 		temp_file = tempfile.NamedTemporaryFile(delete=False)
 		command = subprocess.Popen(['dmidecode'], stdout=subprocess.PIPE)
 		output = command.stdout.read()
@@ -25,13 +57,26 @@ def get_bios_info(target, local):
 		dmi_file = target + 'dmidecode'
 	
 	if os.path.isfile(dmi_file):
+		# first get the static information
+		
+		bios_info = parse_dmi(dmi_file, 'BIOS')
+		sys_info = parse_dmi(dmi_file, 'System')
+		cpu_info = parse_dmi(dmi_file, 'Processor')
+		
+		bios_vendor = bios_info['Vendor']
+		bios_date = bios_info['Release Date']
+		bios_ver = bios_info['Version']
+		
+		sys_vendor = sys_info['Manufacturer']
+		sys_type = sys_info['Product Name']
+		sys_uuid = sys_info['UUID']
+		
+		cpu_family = cpu_info['Family']
+		cpu_ver = cpu_info['Version']
+		
 		with open(dmi_file, 'r') as dfile:
-			# first the main iterables that have distinct leading names
+			# main iterables that have distinct leading names
 			for line in dfile:
-				if 'Vendor:' in line:
-					bios_vendor = line.split()[1]
-				if 'Release Date:' in line:
-					bios_date = line.split()[2]
 				if 'Thread Count:' in line:
 					total_threads += int(line.split()[2])
 					thread_count = line.split()[2]
@@ -55,35 +100,6 @@ def get_bios_info(target, local):
 					cpu_freq = line.split()[2]
 				if 'Physical Memory Array' in line:
 					mem_arrays +=  1
-				if 'Product Name:' in line:
-					index = line.find(':')
-					server_type = line[index+1:len(line)].rstrip('\n')
-				if 'UUID:' in line:
-					serv_uuid = line.split()[1]
-			# now, some ugly badness to get items that have the same leading header
-				if 'Family:' in line:
-					if fam_count == 0:
-						fam_count += 1
-					else:
-						cpu_family = line.split()[1]
-						
-						
-			# this. this right here is horrible. I'm bad and I should feel bad.
-			# ...and also I'm lazy.
-				if 'Version:' in line and 'Specification Version:' not in line:
-					if ver_count == 0:
-						bios_ver = line.split()[1]
-						ver_count +=  1
-					elif ver_count == 1:
-						sys_ver = line.split()[1]
-						ver_count += 1
-					elif ver_count == 2:
-						chas_ver = line.split()[1]
-						ver_count += 1
-					else:
-						index = line.find(':')
-						cpu_ver = line[index+1:len(line)].strip()
-						
 						
 		mem_max_format = int((int(max_mem.split()[0]) * mem_arrays))
 		if mem_max_format >= 1024:
@@ -96,14 +112,14 @@ def get_bios_info(target, local):
 		print '\t\t' + colors.BLUE + 'Version : ' + colors.ENDC + bios_ver
 		print '\t\t' + colors.BLUE + 'Release : ' + colors.ENDC + bios_date
 		print '\t' + colors.HEADER_BOLD + 'System' + colors.ENDC
-		print '\t\t' + colors.BLUE + 'Vendor  : ' + colors.ENDC + bios_vendor
-		print '\t\t' + colors.BLUE + 'Server  :' + colors.ENDC + server_type
-		print '\t\t' + colors.BLUE + 'UUID    : ' + colors.ENDC + serv_uuid
+		print '\t\t' + colors.BLUE + 'Vendor  : ' + colors.ENDC + sys_vendor
+		print '\t\t' + colors.BLUE + 'Server  : ' + colors.ENDC + sys_type
+		print '\t\t' + colors.BLUE + 'UUID    : ' + colors.ENDC + sys_uuid
 		print '\t' + colors.HEADER_BOLD + 'CPU' + colors.ENDC
 		print '\t\t' + colors.WHITE + '{} CPU sockets populated, {} cores / {} threads per core'.format(cpus_populated, core_count, thread_count) + colors.ENDC
 		print '\t\t' + colors.WHITE + '{} total cores - {} total threads'.format(total_cores, total_threads) + colors.ENDC
 		print '\t\t' + colors.BLUE + 'Family  : ' + colors.ENDC + cpu_family
-		print '\t\t' + colors.BLUE + 'Clock   : ' + colors.ENDC + cpu_freq 
+		print '\t\t' + colors.BLUE + 'Clock   :  ' + colors.ENDC + cpu_freq 
 		print '\t\t' + colors.BLUE + 'Model   : ' + colors.ENDC + cpu_ver
 		print '\t' + colors.HEADER_BOLD + 'Memory' + colors.ENDC
 		print '\t\t' + colors.WHITE + '{} of {} DIMMs populated'.format((dimm_count - empty_dimms), dimm_count) + colors.ENDC
