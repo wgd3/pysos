0f84c17d97819317f16275337b51ed09316e3597
Jake <jhunsaker@redhat.com>
2014-07-04 17:17:24 -0400

    Now baked with 80% less badness - more accurate results

--- pysos/opsys/findrpm.py
+++ pysos/opsys/findrpm.py
@@ -1,9 +1,11 @@
 import os
 
 def find_rpm(target, rpm, local=False):
+	
 	# from here we will pull specific targets for installed RPMS
-	rpm_state = False
+	rpm_state = []
 	if local:
+		import tempfile, subprocess
 		temp_file = tempfile.NamedTemporaryFile(delete=False)
 		command = subprocess.Popen(['rpm', '-qa', rpm], stdout=subprocess.PIPE)
 		output = command.stdout.read()
@@ -11,7 +13,6 @@ def find_rpm(target, rpm, local=False):
 			temp_file.write(line)
 		temp_file.close()
 		rpm_file = temp_file.name
-
 		
 	else:
 		rpm_file = target + 'installed-rpms'
@@ -20,10 +21,15 @@ def find_rpm(target, rpm, local=False):
 		with open(rpm_file, 'r') as rpmfile:
 			for line in rpmfile:
 				if line.startswith(rpm):
-					rpm_state = line.split()[0]
-					return rpm_state
+					index = line.find('.')
+					this_rpm = line[0:index-2]
+					if this_rpm == rpm:
+						return line.split()[0]
+			
 		if not rpm_state:
 			return 'not installed'
+		else:
+			return rpm_state
 	else:
 		return 'installed-rpms not found'
 	
