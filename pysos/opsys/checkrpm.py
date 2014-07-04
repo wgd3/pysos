7491d71246945f8e5d2686bae53ae3b2fa875ab8
Jake <jhunsaker@redhat.com>
2014-07-04 14:47:08 -0400

    Added lost check_installed() and corrected check_rpm() to search for entire version string

--- pysos/opsys/checkrpm.py
+++ pysos/opsys/checkrpm.py
@@ -1,18 +1,13 @@
 import json, urllib2
 
-def check_rpm_ver(rpm, stand_alone=False, all_test=False, name=False, ver=False):
+def check_rpm_ver(rpm, stand_alone=False, all_test=False):
+
 	index = rpm.find('.')
-	index2 = rpm.find('.el6')
-	if not name:
-		rpmname = rpm[0:index-2]
-	else:
-		rpmname = name
-	if not ver:
-		version = rpm[index-1:index2]
-	else:
-		version = ver
+	rpmname = rpm[0:index-1]
+
+	version = rpm[len(rpmname):len(rpm)]
 	base_url = "http://pysosweb-wdaniel.itos.redhat.com/"
-	check_url = base_url + 'check/' + rpmname + '/' + version
+	check_url = base_url + 'check/' + rpmname.strip('-') + '/' + version
 	
 	try:
 		rpm_check = urllib2.urlopen(check_url)
