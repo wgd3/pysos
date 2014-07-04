4bee5342b3b4f54738ff9019824e2e1da97ff3c5
Jake <jhunsaker@redhat.com>
2014-07-04 12:35:46 -0400

    Logic now handles stacked taint codes

--- pysos/kernel/taintcheck.py
+++ pysos/kernel/taintcheck.py
@@ -1,28 +1,44 @@
+from collections import OrderedDict
+
 def taint_check(target):
 	
 	# check for kernel taint, well known values go in this dictionary
-	t={}
-	t['0']="Not tainted. Hooray!"
-	t['1']="Proprietary module has been loaded"
-	t['2']="Module has been forcibly loaded"
-	t['4']="SMP with CPUs not designed for SMP"
-	t['8']="User forced a module unload"
-	t['16']="System experienced a machine check exception"
-	t['32']="System has hit bad_page"
-	t['64']="Userspace-defined naughtiness"
-	t['128']="Kernel has oopsed before"
-	t['256']="ACPI table overridden"
-	t['512']="Taint on warning"
-	t['1024']="Modules from drivers/staging are loaded"
-	t['2048']="Working around severe firmware bug"
-	t['4096']="Out-of-tree module has been loaded"
-	t['268435456']="Hardware is unsupported"
+	t=OrderedDict()
 	t['536870912']="Technology Preview code is loaded"
+	t['268435456']="Hardware is unsupported"
+	t['4096']="Out-of-tree module has been loaded"
+	t['2048']="Working around severe firmware bug"
+	t['1024']="Modules from drivers/staging are loaded"
+	t['512']="Taint on warning"
+	t['256']="ACPI table overridden"
+	t['128']="Kernel has oopsed before"
+	t['64']="Userspace-defined naughtiness"
+	t['32']="System has hit bad_page"
+	t['16']="System experienced a machine check exception"
+	t['8']="User forced a module unload"
+	t['4']="SMP with CPUs not designed for SMP"
+	t['2']="Module has been forcibly loaded"
+	t['1']="Proprietary module has been loaded"
+	t['0']="Not tainted. Hooray!"
 	
-	# add logic for stacked codes later
+
 	with open(target + 'proc/sys/kernel/tainted', 'r') as tfile:
 		check = tfile.read().splitlines()
-		if check[0] in t:
-			return t[check[0]]
+		check = check[0]
+		if check in t:
+			return t[check]
 		else:
-			return 'Undefined taint code : %s' %check[0]
+			check = int(check)
+			taint_string = ' '
+			for key in t:
+				if int(check) - int(key) > int('-1'):
+					taint_string = taint_string + t[key] + '\n\t\t\t     '
+					check = int(check) - int(key)
+					if check == 0:
+						return taint_string
+				else:
+					pass
+			# we should only hit this if we have an undefined taint code remainder from the above
+			return taint_string + '\n\t\t\t     Undefined taint code: %s' %str(check)
+
+
