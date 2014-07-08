30343c79a87de2d37519645333f270c6acd2d2ef
Jake <jhunsaker@redhat.com>
2014-07-08 15:26:20 -0400

    Adjusted formatting. Added 'Ds' to D state proc list

--- pysos/opsys/ps.py
+++ pysos/opsys/ps.py
@@ -21,16 +21,17 @@ def get_ps_info(target):
 			usage[value[0]] = [float(value[2]), float(value[3]), float(value[5])]
 	
 	print '\t' + colors.WHITE + 'Top Users of CPU and MEM : ' + colors.ENDC
-	print '\t ' + colors.BLUE + colors.BOLD + '{:7}  {:5}  {:5}  {:7}'.format('USER', '%CPU', '%MEM', 'RSS') + colors.ENDC
+	print '\t ' + colors.BLUE + colors.BOLD + '{:10}  {:6}  {:6}  {:8}'.format('USER', '%CPU', '%MEM', 'RSS') + colors.ENDC
 	report_usage = sorted(usage.items(), reverse=True, key=lambda x: x[1][2])
 	for i in xrange(0,4):
 		value = report_usage[i]
-		print '\t {:<8}  {:<5} {:^5}  {:5.2f} GB'.format(value[0], value[1][0], value[1][1], int(value[1][2]) / 1048576)
+		print '\t {:<10}  {:^6.2f}  {:^6.2f}  {:>3.2f} GB'.format(value[0], value[1][0], value[1][1], int(value[1][2]) / 1048576)
 	print ''
+	
 	# search for defunct and trouble processes
 	for item in retval:
 		value = retval[retval.index(item)]
-		if any('<defunct>' in item for item in value) or any('D' in item for item in value[7]):
+		if any('<defunct>' in item for item in value) or any('D' in item for item in value[7]) or any('Ds' in item for item in value[7]):
 			bad_procs += 1
 			bad_proc[bad_procs] = value
 	if bad_procs > 0:
@@ -77,5 +78,5 @@ def print_ps_info(retval, num, single=False):
 				cmd = cmd + ' ' + str(ps[i]).strip('\n')
 			except:
 				pass
-		print '\t{:^8} {:^6}\t{:^5} {:^5}  {:<7.0f}  {:<7.0f}  {:^5} {:^4} {:^6} {:<7}{}'.format(ps[0], ps[1], ps[2], ps[3], ps[4], ps[5], ps[6], ps[7], ps[8], ps[9], cmd[0:50])
+		print '\t{:^8} {:^6}\t{:^5} {:^5}  {:<7.0f}  {:<7.0f}  {:^5} {:^4} {:^6} {:<9}{}'.format(ps[0], ps[1], ps[2], ps[3], ps[4], ps[5], ps[6], ps[7], ps[8], ps[9], cmd[0:60])
 	print ''
